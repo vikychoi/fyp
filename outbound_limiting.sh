ddbf9847a8c879deddd7c9e845bc1f26412c4537
jasperkee <jasperkee727@gmail.com>
2021-01-21 22:30:32 +0800

    iptable scripted added, limiting traffic at 60 packet per IP, ELK_IP is 52.246.186.129

---
+++ outbound_limiting.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#check if is root
+if [ `id -u` -ne 0 ]; then
+   echo "This script requires root access."
+   exit 1
+fi
+
+ELK_IP=52.246.186.129
+packet_per_sec=60
+
+# defining function
+remap_tcp_port() {
+    #this line route packet receive on port 22 to port 3000
+    iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-ports 3000
+
+    #this line route internal packet from port 22 to port 3000
+    #iptables -t nat -A OUTPUT -p tcp -d localhost --dport 22 -j REDIRECT --to-ports 3000
+}
+
+# defining function
+outbound_restrict() {
+    #max $1 outbound packets per IP
+    iptables -A OUTPUT -p tcp -d $ELK_IP -j ACCEPT
+    iptables -A OUTPUT -p tcp -m hashlimit --hashlimit-name outbound_limit --hashlimit-upto $1/sec --hashlimit-mode dstip -j ACCEPT
+    iptables -A OUTPUT -p tcp -j DROP
+}
+
+# allowing port forwarding
+#sysctl -w net.ipv4.ip_forward=1
+
+# call the function and forward port 22 to 3000
+#remap_tcp_port 
+
+#flush all existing outbound rules
+iptables -F OUTPUT
+
+# restict outbound traffic, the parameter is the max outbound packets per IP
+outbound_restrict $packet_per_sec
